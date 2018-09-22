b4e4585631ab76d4afddd5657edf4bb4d7b36b12
kwetz903 <kwetz903@students.bju.edu>
2018-09-22 09:01:53 -0400

    temp

--- check_hunters/core/settings.py
+++ check_hunters/core/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 'users',  TODO
-    'accounts', 
+    'accounts',
+    'checks', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- check_hunters/core/urls.py
+++ check_hunters/core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     re_path(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     re_path(r'admin/', admin.site.urls),
     re_path(r'accounts/', include('accounts.urls')),
+    re_path(r'accounts/', include('checks.urls')),
 ]
