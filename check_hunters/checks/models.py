990fbfe7f9b796f1fcc88d76e61e8cdf5186c456
kwetz903 <kwetz903@students.bju.edu>
2018-09-26 13:00:11 -0400

    changed check filteR

--- check_hunters/checks/models.py
+++ check_hunters/checks/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django import forms
 import django_filters
 from accounts.models import Account, Client
+from django.forms import SelectDateWidget
 
 def validate_positive(num):
     return num >= 0
@@ -36,9 +37,12 @@ class Check(models.Model):
 
 class CheckFilter(django_filters.FilterSet):
     """class to filter checks for reports"""
-    made_date = django_filters.DateRangeFilter(label='Check Made Out Date')
-    paid_date = django_filters.DateRangeFilter(label='Check Paid Date')
+    made_date_start = django_filters.DateFilter(label='Made Out Start Date', field_name='made_date', lookup_expr='gte', widget=SelectDateWidget)
+    made_date_end = django_filters.DateFilter(label='Made Out End Date', field_name='made_date', lookup_expr='lte', widget=SelectDateWidget)
     
+    paid_date_start = django_filters.DateFilter(label='Paid Start Date',field_name='paid_date', lookup_expr='gte', widget=SelectDateWidget)
+    paid_date_end = django_filters.DateFilter(label='Paid End Date', field_name='paid_date', lookup_expr='lte', widget=SelectDateWidget)
+
     class Meta:
         model = Check
-        fields = ['made_date', 'paid_date']
+        fields = []
