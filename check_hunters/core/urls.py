4559ebd4262aa72c8c1c4aee625a57bb209cdb4c
cshea892 <cshea892@students.bju.edu>
2018-10-09 21:02:39 -0400

    added some homepage styling

--- check_hunters/core/urls.py
+++ check_hunters/core/urls.py
@@ -18,7 +18,7 @@ from django.urls import re_path, include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    re_path(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    re_path(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     re_path(r'admin/', admin.site.urls),
     re_path(r'accounts/', include('accounts.urls')),
     re_path(r'checks/', include('checks.urls')),
