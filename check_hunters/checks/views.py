3937e339e697374336392c0cb3c9c0c3461e8341
unknown <skatt621@students.bju.edu>
2018-11-15 10:23:47 -0500

    changed checks\views.py again 3

--- check_hunters/checks/views.py
+++ check_hunters/checks/views.py
@@ -127,7 +127,7 @@ def current_datetime(request):
     # resultchck = subprocess.run(['python', 'manage.py', 'test', 'checks'], stderr=subprocess.PIPE)
     # stringchck = ''.join(resultchck.stderr.decode('utf-8'))
     ##
-    resultchck = subprocess.run(['python', 'Brogrammers\check_hunters\manage.py', 'test', 'checks'], stderr=subprocess.PIPE)
+    resultchck = subprocess.run(['python', 'Brogrammers\check_hunters\manage.py', 'test', 'Brogrammers\check_hunters\checks'], stderr=subprocess.PIPE)
     stringchck = ''.join(resultchck.stderr.decode('utf-8'))
     ##
     index = stringchck.find("Ran")
