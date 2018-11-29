87e412a7a27a2d4c5cd62e46fffd9dce86ebcec2
kwetz903 <kwetz903@students.bju.edu>
2018-11-29 10:37:18 -0500

    Update settings.py

--- check_hunters/core/settings.py
+++ check_hunters/core/settings.py
@@ -168,7 +168,7 @@ LOGGING = {
         'file': {
             'level': 'INFO',
             'class': 'logging.FileHandler',
-            'filename': './logs/debug.log',
+            'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
         },
     },
     'loggers': {
