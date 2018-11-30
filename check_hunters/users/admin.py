b33d24b195b8db019aeb4c857454e85fed1db567
kwetz903 <kwetz903@students.bju.edu>
2018-11-30 00:37:44 -0500

    enabled changing user client

--- check_hunters/users/admin.py
+++ check_hunters/users/admin.py
@@ -6,12 +6,19 @@ from django.contrib.admin.models import LogEntry
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
+from django.utils.translation import gettext, gettext_lazy as _
 
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ['username', 'email', 'client']
     
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-
+    # add_form = CustomUserCreationForm
+    # form = CustomUserChangeForm
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'client')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
 admin.site.register(User, CustomUserAdmin)
