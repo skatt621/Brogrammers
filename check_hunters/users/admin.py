590e9bfcf9a1b8c9c2f9164fcd0656dc74cd399a
kwetz903 <kwetz903@students.bju.edu>
2018-11-30 00:40:34 -0500

    enabled specifying user client at creation

--- check_hunters/users/admin.py
+++ check_hunters/users/admin.py
@@ -21,4 +21,10 @@ class CustomUserAdmin(UserAdmin):
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'client', 'password1', 'password2'),
+        }),
+    )
 admin.site.register(User, CustomUserAdmin)
